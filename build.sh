3b469bc18be3fc608eecd358f82529832a8192ff
TokiZeng <137824202+TokiZeng@users.noreply.github.com>
2024-02-24 04:57:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@
 
 apt update && apt upgrade
 pkg install automake clang git vim cmake
-chmod +x start.sh
+chmod +x start
 git clone https://github.com/xmrig/xmrig
 cd xmrig
 mkdir build && cd build
 cmake .. -DWITH_HWLOC=OFF
 make -j$(nproc)
 cd ~
-cp termux-xmrig/start.sh ~
-rm termux-xmrig/start.sh
+cp termux-xmrig/start ~
+rm termux-xmrig/start
 cd
