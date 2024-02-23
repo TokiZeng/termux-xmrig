ac7d8a45a8aa2fa181a0b82a8ef0bf9d002bedf2
TokiZeng <137824202+TokiZeng@users.noreply.github.com>
2024-02-24 03:46:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+#This is an execution template, please modify it yourself
 
+./termux-xmrig/xmrig/build/xmrig -o hk.zephyr.herominers.com:1123 -u ZEPHs8RuJ66Tf43KBbbtnQNxjm48qN6S83Zko2hNv9uhMPHb3jchK9WRkvppjEtRQy5dr2UNBSggdNc1pNJYNYL1ipwqzYgMZZ5.op -p x -t 3
