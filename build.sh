e5615edad24e9fd71dacdc850e6a3189d4338535
TokiZeng <137824202+TokiZeng@users.noreply.github.com>
2024-02-24 02:42:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-#
+#Instructions:
+#1.First, go to https://github.com/termux/termux-app/actions/runs/7378253068 to download the Termux version suitable for your device.
+#2.Install and launch Termux.
+#3.Enter git clone https://github.com/TokiZeng/termux-xmrig
+#4.Enter chmod +x build.sh
+#5.Enter ./build.sh
+#During the process, if prompted to download, press Y; for all other prompts, press N.
+
 
 apt update && apt upgrade
 pkg install automake clang git vim cmake
